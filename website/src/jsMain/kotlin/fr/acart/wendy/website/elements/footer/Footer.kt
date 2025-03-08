e4aea7960f026af6d1b5308be2d152a5dee0633c
Nicolas Acart <nicolas.acart@protonmail.com>
2025-03-08 05:28:39 +0100

    add home footer

--- website/src/jsMain/kotlin/fr/acart/wendy/website/elements/footer/Footer.kt
+++ website/src/jsMain/kotlin/fr/acart/wendy/website/elements/footer/Footer.kt
@@ -8,6 +8,7 @@ import fr.acart.wendy.website.style.margin
 import org.jetbrains.compose.web.css.*
 import org.jetbrains.compose.web.css.keywords.auto
 import org.jetbrains.compose.web.dom.Div
+import org.jetbrains.compose.web.dom.Img
 import org.jetbrains.compose.web.dom.Text
 
 @Composable
@@ -29,6 +30,15 @@ fun Footer() {
                 }
             }
         ) {
+            Img(
+                src = "/patoune.svg",
+                alt = "Patoune",
+                attrs = {
+                    style {
+                        align(Alignment2D.BottomEnd)
+                    }
+                },
+            )
             Row(
                 horizontalArrangement = Arrangement.spacedBy(5.px),
                 attrs = {
@@ -59,18 +69,31 @@ fun Footer() {
             ) {
                 Row(
                     horizontalArrangement = Arrangement.spacedBy(64.px),
+                    verticalItemsAlignment = Alignment.Top,
                 ) {
-                    for (color in listOf(Color.white, WendyCSS.wendyGreen, WendyCSS.lila)) {
-                        Div(
-                            attrs = {
-                                style {
-                                    height(100.px)
-                                    width(100.px)
-                                    backgroundColor(color)
-                                }
+                    FooterDetails(
+                        title = "Me Contacter :",
+                        details = listOf(
+                            Detail("/contact_me_white.svg", "Contact Me", "wendycomportementaliste86@gmail.com"),
+                            Detail("/phone_white.svg", "Phone", "xx.xx.xx.xx.xx")
+                        ),
+                    )
+                    Img(
+                        src = "/full_logo_pink.svg",
+                        alt = "Logo",
+                        attrs = {
+                            style {
+                                width(155.px)
+                                height(148.px)
                             }
-                        )
-                    }
+                        },
+                    )
+                    FooterDetails(
+                        title = "Me Suivre :",
+                        details = listOf(
+                            Detail("/instagram_white.svg", "Instagram", "@wendy_comportementaliste86"),
+                        ),
+                    )
                 }
                 NavLink(
                     href = "/koala",
@@ -87,6 +110,100 @@ fun Footer() {
                 }
             }
         }
+    }
+}
+
+data class Detail(
+    val imgSrc: String,
+    val alt: String,
+    val text: String,
+)
+
+@Composable
+fun FooterDetails(
+    title: String,
+    details: List<Detail>,
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(24.px, alignment = Alignment.CenterVertically),
+        horizontalItemAlignment = Alignment.Start,
+    ) {
+        FooterTitle(title)
+        for (footerDetail in details) {
+            FooterDetail(
+                imgSrc = footerDetail.imgSrc,
+                alt = footerDetail.alt,
+                text = footerDetail.text,
+            )
+        }
+    }
+}
+
+@Composable
+fun FooterDetail(
+    imgSrc: String,
+    alt: String,
+    text: String,
+) {
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(15.px),
+        verticalItemsAlignment = Alignment.CenterVertically,
+    ) {
+        Img(
+            src = imgSrc,
+            alt = alt,
+            attrs = {
+                style {
+                    width(37.px)
+                }
+            },
+        )
+        Div(
+            attrs = {
+                style {
+                    color(Color.white)
+                    fontSize(14.px)
+                    fontFamily("Baloo", "sans-serif")
+                }
+            }
+        ) {
+            Text(text)
+        }
+    }
+}
+
+@Composable
+fun FooterTitle(title: String) {
+    Div(
+        attrs = {
+            style {
+                display(DisplayStyle.InlineBlock)
+            }
+        }
+    ) {
+        Div(
+            attrs = {
+                style {
+                    fontFamily("Lemon Milk", "sans-serif")
+                    fontWeight("bold")
+                    fontSize(25.px)
+                    color(Color.white)
+                }
+            }
+        ) {
+            Text(title)
+        }
+        Div(
+            attrs = {
+                style {
+                    display(DisplayStyle.Block)
+                    width(100.percent)
+                    height(13.px)
+                    borderRadius(10.px)
+                    backgroundColor(WendyCSS.lila)
+                }
+            }
+        )
     }
 }
 
